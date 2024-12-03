c134f4ee0ef16a2b325c4eb4be500b6f5e5106f1
Emily Palaska <aimilia.p2@gmail.com>
2024-12-03 13:30:38 +0200

    update main to match new file names

--- llm_blockmerger/main_encode.py
+++ llm_blockmerger/main_encode.py
@@ -1,5 +1,5 @@
-from loading.BlockLoader import BlockLoader
-from encoding.EmbeddingModel import EmbeddingModel
+from loading.blockloader import BlockLoader
+from encoding.embeddingmodel import EmbeddingModel
 
 def main():
     notebook_paths = ['notebooks/02.02-The-Basics-Of-NumPy-Arrays.ipynb']
@@ -7,12 +7,12 @@ def main():
     print('BLOCKS')
     print(len(block_loader.blocks))
 
-
+    """
     for i, label in enumerate(block_loader.labels):
         print(label)
         #print(block_loader.blocks[i])
         print('-' * 40)
-
+    """
 
     embedding_model = EmbeddingModel()
     embedding_model.encode(block_loader.labels)
