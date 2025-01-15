ba4653e13950b0cbc8b9e68cac9f95c115998e1c
Emily Palaska <aimilia.p2@gmail.com>
2025-01-15 14:56:19 +0200

    implement embedding tests

---
+++ tests/test_embedding_model.py
@@ -0,0 +1,14 @@
+import pytest, sys
+sys.path.append("..")
+from llm_blockmerger.encoding.embedding_model import *
+
+@pytest.fixture
+def labels():
+    return ['label 1', 'label 2', 'label 3', 'label 4', 'label 5']
+
+def test_embedding_model_default(labels):
+    embedding_model = initialize_model()
+    assert isinstance(embedding_model, SentenceTransformer)
+    embeddings = encode_labels(embedding_model, labels)
+    for embedding in embeddings:
+        assert embedding.shape == (384,)
