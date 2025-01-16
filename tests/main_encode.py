65e504bd838d196988a50550d88157694e22bc76
Emily Palaska <aimilia.p2@gmail.com>
2025-01-16 18:13:35 +0200

    remove unnecessary comments

--- tests/main_encode.py
+++ tests/main_encode.py
@@ -12,15 +12,6 @@ def main():
     blocks, labels = concatenate_managers(preprocess_blocks(load_notebooks(notebook_paths)))
     assert len(blocks) == len(labels), "Blocks and labels should have the same length"
 
-    """
-    # Prints the extracted blocks
-    for block, l abel in zip(blocks, labels):
-        print(label)
-        print('CODE:')
-        print(block)
-        print('-'*40)
-    """
-
     # Embed and visualize labels
     embedding_model = initialize_model()
     embeddings = encode_labels(embedding_model, labels)
@@ -28,24 +19,13 @@ def main():
     print('EMBEDDINGS:', embeddings.shape)
 
     # Create a vector database, and try it with an add/read example
-    # TODO test assertation of shape with add/read example
     example = 'numpy'
     vector_db = (VectorDB(dbtype=HNSWVectorDB))
     vector_db.create(labels, embeddings, blocks)
     print('MATCHES: ', len(vector_db.read(encode_labels(embedding_model, [example]))))
 
-    """
-    # Prints the labels and code blocks of the matches
-    for m in matches:
-        print('-' * 40)
-        print(m.label)
-        print(m.block)
-        print('-'*40)
-    """
-
     # Code generation pipeline through a linear search
     example = 'simple numpy program'
-    # Choice of merging method: string or embedding comparison
     #labels, blocks = linear_string_merge(embedding_model, vector_db, example)
     labels, blocks = linear_embedding_merge(embedding_model, vector_db, example)
     print('MERGE RESULT:')
