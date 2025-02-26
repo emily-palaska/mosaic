041e4651eeecd88cace7cf97ceae3f3aa15dbbfe
Emily Palaska <aimilia.p2@gmail.com>
2025-02-26 12:19:49 +0200

    integrate variable extraction to block loading

--- llm_blockmerger/loading/blockloading.py
+++ llm_blockmerger/loading/blockloading.py
@@ -1,22 +1,19 @@
 import json
+from loading.variable_extracting import extract_variables
 
 class CodeBlocksManager:
     def __init__(self, blocks=None, labels=None, variables=None, var_descriptions=None):
-        self.labels = labels
-        self.blocks = blocks
-        self.variables = variables
-        self.var_descriptions = var_descriptions
-
-    def set_values(self, labels=None, blocks=None, variables=None, var_descriptions=None):
-        if labels: self.labels = labels
-        if blocks: self.blocks = blocks
-        if variables: self.variables = variables
-        if var_descriptions: self.var_descriptions = var_descriptions
+        if blocks is None:
+            blocks = []
+        self.labels = labels if labels else []
+        self.blocks = blocks if blocks else []
+        self.variables = variables if variables else set()
+        self.var_descriptions = var_descriptions if var_descriptions else []
 
 def load_notebooks(nb_paths):
     return [json.load(open(path, 'r')) for path in nb_paths]
 
-def extract_cell_content(notebook):
+def _extract_cell_content(notebook):
     code_lines, accumulated_markdown = [], []
     cell_markdown = ''
 
@@ -29,7 +26,7 @@ def extract_cell_content(notebook):
             cell_markdown = ''
     return code_lines, accumulated_markdown
 
-def preprocess_code_lines(code_lines, accumulated_markdown):
+def _preprocess_code_lines(code_lines, accumulated_markdown):
     blocks, labels = [], []
 
     for i, code in enumerate(code_lines):
@@ -40,8 +37,9 @@ def preprocess_code_lines(code_lines, accumulated_markdown):
             if '#' in line:  # Comment detected
                 if not line.startswith('#'):  # Side-comment
                     before_hash, after_hash = line.split('#', 1)
-                    blocks.append([before_hash.strip()])
+                    blocks.append([before_hash])
                     labels.append(f"MARKDOWN: {md}\nCOMMENT: {after_hash.strip()}")
+                    current_block.append(before_hash)
                 else:  # Full-line
                     if current_block:
                         blocks.append(current_block if isinstance(current_block, list) else [current_block])
@@ -49,7 +47,7 @@ def preprocess_code_lines(code_lines, accumulated_markdown):
                     _, current_label = line.split('#', 1)
                     current_block = []
             else:  # Code without comment
-                current_block.append(line.strip())
+                current_block.append(line)
 
         if current_block:  # Handle remaining lines in the current block
             blocks.append(current_block)
@@ -59,13 +57,21 @@ def preprocess_code_lines(code_lines, accumulated_markdown):
 def preprocess_blocks(notebook_data):
     block_managers = []
     for notebook in notebook_data:
-        code_lines, accumulated_markdown = extract_cell_content(notebook)
-        block_managers.append(CodeBlocksManager(*preprocess_code_lines(code_lines, accumulated_markdown)))
+        code_lines, accumulated_markdown = _extract_cell_content(notebook)
+        block_managers.append(CodeBlocksManager(*_preprocess_code_lines(code_lines, accumulated_markdown)))
+        for block in block_managers[-1].blocks:
+            try: block_managers[-1].variables.update(extract_variables(_concatenate_block(block)))
+            except IndentationError: continue
+            except Exception: raise
     return block_managers
 
+def _concatenate_block(block):
+    return '\n'.join(block) + '\n'
+
 def concatenate_managers(block_managers):
-    labels, blocks = [], []
+    labels, blocks, variables = [], [], []
     for block_manager in block_managers:
         labels.extend(block_manager.labels)
         blocks.extend(block_manager.blocks)
-    return blocks, labels
+        variables.append(block_manager.variables)
+    return blocks, labels, variables
