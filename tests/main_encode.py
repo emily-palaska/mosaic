d46510b0cee71fb8ae4083b428dbaba794cf1516
Emily Palaska <aimilia.p2@gmail.com>
2025-02-26 12:18:30 +0200

    clear data and execution scenario

--- tests/main_encode.py
+++ tests/main_encode.py
@@ -1,16 +1,17 @@
 import os
 os.chdir("../")
 
-from llm_blockmerger.loading.blockloading import *
-from llm_blockmerger.encoding.vector_db import *
-from llm_blockmerger.encoding.embedding_model import *
-from llm_blockmerger.linear_merging import *
+from llm_blockmerger.loading.blockloading import concatenate_managers, preprocess_blocks, load_notebooks
+from llm_blockmerger.encoding.vector_db import VectorDB, HNSWVectorDB
+from llm_blockmerger.encoding.embedding_model import initialize_model, encode_labels
+from llm_blockmerger.linear_merging import linear_embedding_merge, linear_string_merge
 
 def main():
     # Load a notebook
     notebook_paths = ['notebooks/02.02-The-Basics-Of-NumPy-Arrays.ipynb']
-    blocks, labels = concatenate_managers(preprocess_blocks(load_notebooks(notebook_paths)))
+    blocks, labels, variables = concatenate_managers(preprocess_blocks(load_notebooks(notebook_paths)))
     assert len(blocks) == len(labels), "Blocks and labels should have the same length"
+    assert len(variables) == len(notebook_paths), "Variables and paths should have the same length"
 
     # Embed and visualize labels
     embedding_model = initialize_model()
@@ -18,14 +19,17 @@ def main():
     #plot_similarity_matrix(compute_similarity(embeddings), './plots/similarity_matrix.png')
     print('EMBEDDINGS:', embeddings.shape)
 
-    # Create a vector database, and try it with an add/read example
-    example = 'numpy'
-    vector_db = (VectorDB(dbtype=HNSWVectorDB))
+    # Create a vector database
+    vector_db = VectorDB(dbtype=HNSWVectorDB, empty=True)
+    assert vector_db.get_size() == 0, 'VectorDB should initialize empty'
     vector_db.create(labels, embeddings, blocks)
-    print('MATCHES: ', len(vector_db.read(encode_labels(embedding_model, [example]))))
-    print(vector_db.get_size())
+    assert vector_db.get_size() == len(blocks), 'VectorDB should have created every block as a vector'
 
-    # Code generation pipeline through a linear search
+    # Read from the database
+    example = 'numpy'
+    print('MATCHES: ', len(vector_db.read(encode_labels(embedding_model, [example]))))
+
+    # Code generation pipeline through a linear string or embedding search
     example = 'simple numpy program'
     #labels, blocks = linear_string_merge(embedding_model, vector_db, example)
     labels, blocks = linear_embedding_merge(embedding_model, vector_db, example)
