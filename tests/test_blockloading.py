57c5f62a1480d9d416b8e472d6dd97f4f68b33d5
Emily Palaska <aimilia.p2@gmail.com>
2025-02-26 12:18:56 +0200

    improve tests for new functionality

--- tests/test_blockloading.py
+++ tests/test_blockloading.py
@@ -3,28 +3,32 @@ from llm_blockmerger.loading.blockloading import *
 
 @pytest.fixture
 def notebook_data():
-    path = './notebooks/test_notebook.ipynb'
+    path = '../notebooks/test_notebook.ipynb'
     blocks = [
-        ['def add(a, b):', 'return a + b'],
-        ['print(result)'],
-        ['result = add(2, 3)'],
-        ['print(result)'],
-        ['result = add(-1, 1)']
+        ['    return a + b '],
+        ['def add(a, b):\n', '    return a + b '],
+        ['print(result)  '],
+        ['result = add(2, 3)\n', 'print(result)  '],
+        ['print(result) '],
+        ['result = add(-1, 1)\n', 'print(result) ']
     ]
 
     labels = [
-        'MARKDOWN: # Functions in python\nSimple addition function\nCOMMENT:  Example of usage:',
+        'MARKDOWN: # Functions in python\nSimple addition function\nCOMMENT: This is a simple addition function',
+        'MARKDOWN: # Functions in python\nSimple addition function\nCOMMENT: Example of usage:',
         'MARKDOWN: Testing it with two examples\nCOMMENT: Should print 5',
         'MARKDOWN: Testing it with two examples\nCOMMENT: ',
         'MARKDOWN: Testing it with two examples\nCOMMENT: Should print 0',
         'MARKDOWN: Testing it with two examples\nCOMMENT: '
     ]
 
-    return [path], blocks, labels
+    variables = [{'a', 'b', 'result'}]
+    return [path], blocks, labels, variables
 
 def test_preprocess_pipeline(notebook_data):
-    path, expected_blocks, expected_labels = notebook_data
-    blocks, labels = concatenate_managers(preprocess_blocks(load_notebooks(path)))
+    path, expected_blocks, expected_labels, expected_variables = notebook_data
+    blocks, labels, variables = concatenate_managers(preprocess_blocks(load_notebooks(path)))
     assert len(blocks) == len(labels)
     assert blocks == expected_blocks
     assert labels == expected_labels
+    assert variables == expected_variables
