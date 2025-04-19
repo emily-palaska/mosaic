30fcc23f52164a16e15e35f316db5795f22c0e96
Emily Palaska <aimilia.p2@gmail.com>
2025-04-19 20:07:22 +0300

    Create version_control.py

---
+++ tests/version_control.py
@@ -0,0 +1,6 @@
+import docarray, pydantic, jina, vectordb
+
+print("docarray:", docarray.__version__)
+print("pydantic:", pydantic.__version__)
+print("jina:", jina.__version__)
+print("vectordb:", vectordb.__version__)  # if available
