04314ac6a2d5da34d925d637ceb99ae75c251b65
Emily Palaska <aimilia.p2@gmail.com>
2025-04-19 20:16:11 +0300

    Delete version_control.py

--- tests/version_control.py
+++
@@ -1,6 +0,0 @@
-import docarray, pydantic, jina, vectordb
-
-print("docarray:", docarray.__version__)
-print("pydantic:", pydantic.__version__)
-print("jina:", jina.__version__)
-print("vectordb:", vectordb.__version__)  # if available
