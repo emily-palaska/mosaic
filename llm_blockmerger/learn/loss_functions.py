2124d85fb7b5cd95970361909c7636a6102baeb1
Emily Palaska <aimilia.p2@gmail.com>
2025-05-05 16:52:49 +0300

    manual cross entropy to avoid logits

--- llm_blockmerger/learn/loss_functions.py
+++ llm_blockmerger/learn/loss_functions.py
@@ -15,14 +15,14 @@ def transitive_contrastive_loss(a, b, c, threshold=0.8):
     loss_similar = (1 - labels) * ca
     loss_dissimilar = labels * (1 - ca)
 
-    #print(f'\t Label distribution: {torch.sum(label, dim=0) / label.shape[0]}')
-    #print(f'\t Similar: {torch.mean(loss_similar)} Dissimilar: {torch.mean(loss_dissimilar)}')
     return torch.mean(0.5 * (loss_similar + loss_dissimilar))
 
 def triplet_cross_entropy_loss(a, b, c, threshold=0.8):
-    ab = F.cosine_similarity(a, b, dim=-1)
-    bc = F.cosine_similarity(b, c, dim=-1)
-    ac = F.cosine_similarity(a, c, dim=-1)
+    ab = normalized_cosine_similarity(a, b)
+    bc = normalized_cosine_similarity(b, c)
+    ac = normalized_cosine_similarity(a, c)
 
     labels = (ab * bc > threshold ** 2).float()
-    return F.cross_entropy(ac, labels)
+    loss = - (labels * torch.log(ac) + (1 - labels) * torch.log(1 - ac))
+
+    return loss.mean()
