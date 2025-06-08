4040e086cf89767f5786d7c4427dea0a82a26746
Emily Palaska <aimilia.p2@gmail.com>
2025-06-08 16:39:46 +0300

    mlp init weights

--- llm_blockmerger/learn/mlp.py
+++ llm_blockmerger/learn/mlp.py
@@ -1,25 +1,28 @@
 import torch
 import torch.nn as nn
+from matplotlib import pyplot as plt
 from torch.utils.data import DataLoader
 from llm_blockmerger.learn.dummy_dataset import DummyTripletDataset
-from llm_blockmerger.learn.loss_functions import transitive_cross_entropy_loss, transitive_contrastive_loss, vector_variance
+from llm_blockmerger.learn.loss_functions import transitive_cross_entropy_loss, transitive_contrastive_loss, vector_variance, pairwise_norm_cos_sim
+
 
 class MLP(nn.Module):
-    def __init__(self, input_dim, layer_dims=None, loss_function=transitive_cross_entropy_loss):
-        assert loss_function in [transitive_contrastive_loss, transitive_cross_entropy_loss], \
-            f'{loss_function} is not a valid loss function'
+    def __init__(self, input_dim, layer_dims=None):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.input_dim = input_dim
         self.layer_dims = layer_dims if layer_dims else [64, 32]
-        self.loss_function = loss_function
 
         super(MLP, self).__init__()
         layers = []
         prev_dim = input_dim
-        for dim in layer_dims:
-            layers.append(nn.Linear(prev_dim, dim))
+        for dim in self.layer_dims:
+            linear = nn.Linear(prev_dim, dim)
+            nn.init.kaiming_uniform_(linear.weight, nonlinearity='relu')
+            nn.init.zeros_(linear.bias)  # Bias init to zero
+            layers.append(linear)
             layers.append(nn.ReLU())
             prev_dim = dim
+
         self.network = nn.Sequential(*layers)
         self.network.to(self.device)
 
@@ -27,12 +30,14 @@ class MLP(nn.Module):
         return self.network(x)
 
 
-def train(model, train_loader, optimizer, epochs=10):
+def train(model, train_loader, optimizer, loss_function, epochs=10):
     model.train()
 
+    all_losses, all_variances = [], []
     for epoch in range(epochs):
         total_loss, num_batches = 0.0, 0
-        total_var = 0.0
+        labels, var = None, None
+
 
         for batch in train_loader:
             a, b, c = batch
@@ -40,22 +45,34 @@ def train(model, train_loader, optimizer, epochs=10):
 
             optimizer.zero_grad()
             a_out, b_out, c_out = model(a), model(b), model(c)
-            loss = model.loss_function(a_out, b_out, c_out)
+
+            loss, labels, var, loss_sim = loss_function(a_out, b_out, c_out)
             assert not torch.isnan(loss).any(), f"Loss is NaN at epoch {epoch + 1}"
             loss.backward()
             optimizer.step()
 
-            total_loss += loss.item()
+            total_loss += loss_sim
 
             num_batches += 1
 
         avg_loss = total_loss / num_batches
-        print(f'\tEpoch [{epoch + 1}/{epochs}]\t Loss: {avg_loss:.4f}')
+        print(f'Epoch [{epoch + 1}/{epochs}]\t Loss: {avg_loss:.4f} Labels: {labels: .4f}\t Variance (c): {var: .4f}')
+        all_losses.append(avg_loss)
+        all_variances.append(var)
+    plt.figure()
+    plt.plot(all_losses)
+    plt.title('Losses')
+    plt.show()
+    plt.figure()
+    plt.plot(all_variances)
+    plt.title('Variances')
+    plt.show()
+
 
 def main():
     feat_dim, num_samples, batch_size, lr, epochs, layer_dims = 128, 1000, 1000, 0.001, 10, [64, 32]
 
-    model = MLP(input_dim=feat_dim, layer_dims=layer_dims, loss_function=transitive_cross_entropy_loss)
+    model = MLP(input_dim=feat_dim, layer_dims=layer_dims)
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     dataset = DummyTripletDataset(num_samples=num_samples, feat_dim=feat_dim)
     train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
--- tests/integration_learning.py
+++ tests/integration_learning.py
@@ -9,19 +9,19 @@ from llm_blockmerger.learn import MLP, train, transitive_cross_entropy_loss, tra
 def main():
     samples = 1000
     loss_function = transitive_cross_entropy_loss
-    layer_dims, lr, batch_size, epochs = [128, 64, 32], 0.001, 1000, 10
+    layer_dims, lr, batch_size, epochs = [128, 64, 32], 0.001, 1000, 50
 
     vector_db = BlockMergerVectorDB(databasetype=HNSWVectorDB, empty=False, training_samples=samples)
     print(f'Initialized vector database with {vector_db.get_num_docs()} entries and {len(vector_db)} training samples...')
 
-    model = MLP(input_dim=vector_db.feature_size, layer_dims=layer_dims, loss_function=loss_function)
+    model = MLP(input_dim=vector_db.feature_size, layer_dims=layer_dims)
     optimizer = optim.Adam(model.parameters(), lr=lr)
     print('Initialized MLP model...')
 
     train_loader = DataLoader(vector_db, batch_size=batch_size, shuffle=False)
     print('Created train loader...')
 
-    train(model, train_loader, optimizer, epochs=epochs)
+    train(model, train_loader, optimizer, loss_function=loss_function, epochs=epochs)
     print('Finished training...')
 
 if __name__ == '__main__':
